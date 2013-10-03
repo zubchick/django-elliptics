d77d579a0132e9fdaccad0e6541b06724d62ee1b
Alexey Mikerin <elisei@yandex-team.ru>
2013-10-03 10:40:15 +0300

    Added changes from the code tools/wiki: timeouts, logging, chunked_encoding

--- django_elliptics/storage.py
+++ django_elliptics/storage.py
@@ -1,34 +1,55 @@
 import requests
 import urllib
+import logging
+import time
+import socket
 from cStringIO import StringIO
+from requests import Timeout
 
 from django import conf
 from django.core.files import base, storage
 
+logger = logging.getLogger(__name__)
+
+
 class BaseError (Exception):
     """Generic error for EllipticsStorage backend."""
+
 
 class ModeError (BaseError):
     """File operation incompatible with file access mode."""
 
+
 class HTTPError (BaseError):
     """Elliptics request failed."""
+
 
 class SaveError (HTTPError):
     """Failed to store file to the backend."""
 
     def __str__(self):
-	response = self.args[0]
+        response = self.args[0]
         return 'got status code %s while sending to %s' % (
             response.status_code, response.url)
+
 
 class ReadError (HTTPError):
     """Failed to read from the backend."""
 
     def __str__(self):
-	response = self.args[0]
+        response = self.args[0]
         return 'got status code %s while reading %s' % (
             response.status_code, response.url)
+
+
+class TimeoutError(ReadError, SaveError):
+    """Timeout error."""
+
+    # ReadError and SaveError override __str__, because they get object with a response to the input.
+    # In TimeoutError is impossible to pass the object with a response, therefore overriding the back.
+    def __str__(self):
+        return super(HTTPError, self).__str__()
+
 
 class EllipticsStorage (storage.Storage):
     """Django file storage backend for Elliptics via HTTP API.
@@ -111,6 +132,7 @@ class EllipticsStorage (storage.Storage):
 
         return url
 
+
 class EllipticsFile (base.File):
     def __init__(self, name, storage, mode):
         self.name = name
@@ -168,3 +190,93 @@ class EllipticsFile (base.File):
 
     def seek(self, offset, mode=0):
         self._stream.seek(offset, mode)
+
+
+class TimeoutAwareEllipticsStorage(EllipticsStorage):
+    timeout_get = getattr(conf.settings, 'ELLIPTICS_GET_CONNECTION_TIMEOUT', 3)
+    retries_get = getattr(conf.settings, 'ELLIPTICS_GET_CONNECTION_RETRIES', 3)
+    timeout_post = getattr(conf.settings, 'ELLIPTICS_POST_CONNECTION_TIMEOUT', 5)
+    retries_post = getattr(conf.settings, 'ELLIPTICS_POST_CONNECTION_RETRIES', 9)
+
+    def _request(self, method, url, *args, **kwargs):
+        if method == 'POST':
+            return self.session.post(url, *args, **kwargs)
+        elif method == 'GET':
+            return self.session.get(url, *args, **kwargs)
+        else:
+            return self.session.head(url, *args, **kwargs)
+
+    def _timeout_request(self, method, url, *args, **kwargs):
+        error_message = ''
+        if method == 'POST':
+            retries = self.retries_post
+            timeout = self.timeout_post
+        else:
+            retries = self.retries_get
+            timeout = self.timeout_get
+
+        for retry_count in xrange(retries):
+            try:
+                started = time.time()
+                response = self._request(method, url, *args, timeout=timeout, **kwargs)
+            except socket.gaierror as exc:
+                raise ValueError('incorrect elliptics request {0} "{1}": {2}'.format(method, url, repr(exc)))
+            except Timeout, exception:
+                error_message = str(exception)
+            else:
+                logger.info('%s %s %s timeout=%s retries=%s time=%.4f',
+                            method, url, args or '', timeout, retry_count, time.time() - started)
+                break
+        else:
+            logger.error('%s failed attempts of %s to connect to Elliptics (%s %s). Timeout: %s seconds. "%s"',
+                         retry_count + 1, retries, method, url, timeout, error_message)
+            raise TimeoutError(error_message)
+
+        if retry_count:
+            logger.warning('%s failed attempts of %s to connect to Elliptics (%s %s). Timeout: %s seconds. "%s"',
+                           retry_count, retries, method, url, timeout, error_message)
+
+        return response
+
+    def _fetch(self, name):
+        url = self._make_private_url('get', name)
+        response = self._timeout_request('GET', url)
+
+        if response.status_code != 200:
+            logger.warning('Elliptics read error status %d, url %s',
+                           response.status_code, url, extra={'stack': True})
+            raise ReadError(response)
+
+        return response.content
+
+    def _save(self, name, content, append=False):
+        args = {}
+        if append:
+            args['ioflags'] = 2  # DNET_IO_FLAGS_APPEND = (1<<1)
+
+        url = self._make_private_url('upload', name, **args)
+        response = self._timeout_request('POST', url, data=content)
+
+        if response.status_code != 200:
+            raise SaveError(response)
+
+        return name
+
+    def _make_url(self, *parts, **args):
+        """
+        Return URL.
+
+        Quotes the path section of a URL.
+        @return: str
+        """
+        if not isinstance(parts, list):
+            parts = list(parts)
+
+        for index in xrange(1, len(parts)):
+            parts[index] = urllib.quote(parts[index])
+
+        url = super(TimeoutAwareEllipticsStorage, self)._make_url(
+            *parts,
+            **args
+        )
+        return url
